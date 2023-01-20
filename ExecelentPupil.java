815e323579c7d2ef37be354e63bdbe1499a9e551
neregist <animetrity@gmail.com>
2023-01-20 12:07:37 +0200

    newuser

---
+++ ExecelentPupil.java
@@ -0,0 +1,23 @@
+package main.java.java_3_Essential.lesson_3.task_2.version_1;
+
+public class ExecelentPupil extends Pupil{
+    @Override
+    void study() {
+        System.out.println("Вмію неперевершено вчитись");
+    }
+
+    @Override
+    void read() {
+        System.out.println("Вмію неперевершено читати");
+    }
+
+    @Override
+    void write() {
+        System.out.println("Вмію неперевершено писати");
+    }
+
+    @Override
+    void relax() {
+        System.out.println("Вмію неперевершено розслаблятись");
+    }
+}
---
+++ GoodPupil.java
@@ -0,0 +1,23 @@
+package main.java.java_3_Essential.lesson_3.task_2.version_1;
+
+public class GoodPupil extends Pupil{
+    @Override
+    void study() {
+        System.out.println("Вмію добре вчитись");
+    }
+
+    @Override
+    void read() {
+        System.out.println("Вмію добре читати");
+    }
+
+    @Override
+    void write() {
+        System.out.println("Вмію добре писати");
+    }
+
+    @Override
+    void relax() {
+        System.out.println("Вмію добре розслаблятись");
+    }
+}
